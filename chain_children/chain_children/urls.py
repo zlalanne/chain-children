037f41877bdff6d31045b806de03cefa1fe3764b
Zack Lalanne <zack.lalanne@gmail.com>
2013-07-27 17:31:14 -0500

    Added first view for application

--- chain_children/chain_children/urls.py
+++ chain_children/chain_children/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
+from chain_children.views import hello
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'chain_children.views.home', name='home'),
-    # url(r'^chain_children/', include('chain_children.foo.urls')),
+# Examples:
+# url(r'^$', 'chain_children.views.home', name='home'),
+# url(r'^chain_children/', include('chain_children.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+# Add the admin/doc line below to enable admin documentation:
+# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+# Add the next line to enable the admin:
+# url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('', ('^hello/$', hello))
